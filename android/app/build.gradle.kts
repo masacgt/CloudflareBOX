0cf58b789e39182af0f89a7d03bc554eabbccd2f
Omasa <317862954+masacgt@users.noreply.github.com>
2026-09-13 15:25:02 +0900

    Mark large-file stability test build as version 0.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.cloudflarebox.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
     buildFeatures { compose = true }
 }
